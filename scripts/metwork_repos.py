db062bd6948146b32c6e51a9df0b4fbb2c0b3159
Fabien MARTY <fabien.marty@gmail.com>
2018-10-12 12:22:15 +0200

    feat: add exact-level option

--- scripts/metwork_repos.py
+++ scripts/metwork_repos.py
@@ -14,10 +14,14 @@ ORG = "metwork-framework"
 TOPICS_TO_EXCLUDE = ["testrepo"]
 
 
-async def getrepos(minimal_level):
+async def getrepos(minimal_level, exact_level):
     repos = []
     async with ClientSession(auth=AUTH, timeout=TIMEOUT) as session:
-        for i in range(minimal_level, 6):
+        if exact_level:
+            max_level = minimal_level + 1
+        else:
+            max_level = 6
+        for i in range(minimal_level, max_level):
             topics_to_include = ["integration-level-%i" % i]
             tmp = await h.github_get_org_repos_by_topic(session,
                                                         ORG,
@@ -26,13 +30,14 @@ async def getrepos(minimal_level):
             repos = repos + tmp
     return repos
 
-parser = argparse.ArgumentParser(description='list metwork-framework repos '
-                                             'with a minimal integration level')
+parser = argparse.ArgumentParser(description='get metwork-framework repos')
 parser.add_argument('--minimal-level', type=int, default=1,
                     help='minimal integration level')
+parser.add_argument('--exact-level', action='store_true',
+                    help='if set the minimal-level must be the exact level')
 args = parser.parse_args()
 loop = asyncio.get_event_loop()
-reply = loop.run_until_complete(getrepos(args.minimal_level))
+reply = loop.run_until_complete(getrepos(args.minimal_level, args.exact_level))
 loop.close()
 for repo in reply:
     print(repo)
